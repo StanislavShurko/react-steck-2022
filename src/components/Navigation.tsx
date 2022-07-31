02a8ca26b0c328e0a68cd5d779f53f4bdcc9ec04
Stanislav <stas.shurko@gmail.com>
2022-07-31 16:43:35 +0200

    debounced and reduxtoolkit

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,7 +6,6 @@ export function Navigation() {
     <div>
       <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
         <h3 className="font-bold">Github Search</h3>
-
         <span>
           <Link to="/" className="mr-2">Home</Link>
           <Link to="/favorites">Favorites</Link>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useState} from 'react';
+import {useSearchUsersQuery} from '../store/github/github.api';
+import {useDebounce} from '../hooks/debounce';
 
 export function HomePage() {
+  const [search, setSearch] = useState('');
+  const debounced = useDebounce(search);
+  const {isLoading, isError, data} = useSearchUsersQuery('stanislav');
+
+  useEffect( () => {
+    console.log(search);
+  }, [debounced]);
+
   return (
-    <div>
-        home
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      { isError && <p className="text-center text-red-600">Something went wrong...</p>}
+
+      <div className="relative w-[560px]">
+        <input
+          type="text"
+          className="border py-2 px-4 w-full h-[42px] mb-2"
+          placeholder="Search for GitHub username..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, vero?
+        </div>
+      </div>
     </div>
   );
 }
