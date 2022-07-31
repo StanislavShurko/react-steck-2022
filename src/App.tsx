d7f76e44004e34e045bd2ee665461371e2fda112
Stanislav <stas.shurko@gmail.com>
2022-07-31 15:27:49 +0200

    base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import {HomePage} from './pages/HomePage';
+import {FavoritesPage} from './pages/FavoritesPage';
+import {Navigation} from './components/Navigation';
 
 function App() {
   return (
-    <div>
-      <h1 className="font-bold">Hello World!</h1>
-    </div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={ <HomePage/> }></Route>
+        <Route path="/favorites" element={ <FavoritesPage/> }></Route>
+      </Routes>
+    </>
   );
 }
 
